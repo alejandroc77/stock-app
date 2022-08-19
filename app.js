5423b024f01b0e1d9d2ad6e4ec3887be3caac8f6
alejandroc77 <cabreraalejandro1828@gmail.com>
2022-08-19 18:25:46 -0500

    Agregamos nuevos estilos y cambios

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-require('dotenv').config()
-const express = require('express')
-const path = require('path')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const app = express();
 
-app.use(express.static(path.join(__dirname,'public')))
+mongoose
+  .connect(
+    `mongodb+srv://Jordy:${process.env.MONGO_DB_PASS}@developer.rnd4oyz.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then((resul) => {
+    app.listen(PORT, () => {
+      console.log(`Escuchando en el puerto ${PORT}`);
+    });
+    console.log("Conexion exitosa a la base de datos");
+  })
+  .catch((err) => console.log(`conexion fallida ${err}`));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 /* app.get('/',(resq, res)=>{
     console.log('peticion recibida');
     res.status(200).sendFile('index.html',{root:__dirname});
+    next()
 }) */
 
-const PORT = process.env.PORT || 4000
- 
-app.listen(PORT, ()=>{
-    console.log(`Escuchando en el puerto ${PORT}`);
-})
+const PORT = process.env.PORT || 4000;
--- public/index.js
+++ public/index.js
@@ -1,1 +1,11 @@
-console.log('hola mundo');
+
+const inputName = document.querySelector('#productName')
+const inputNumber = document.querySelector('#productNumber')
+
+console.log({inputName,inputNumber});
+
+const boton = document.querySelector('button');
+
+boton.addEventListener('click',(e) =>{
+        console.log({nombre:inputName.value, precio:inputNumber.value });
+})

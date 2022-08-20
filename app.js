d41736a8112a8041e8281cd4f88e3e8f82795766
alejandroc77 <cabreraalejandro1828@gmail.com>
2022-08-20 11:06:31 -0500

    Conexion exitosa

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 mongoose
   .connect(
-    `mongodb+srv://Jordy:${process.env.MONGO_DB_PASS}@developer.rnd4oyz.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://Jordy:${process.env.MONGO_DB_PASS}@developer.rnd4oyz.mongodb.net/stock-jordy?retryWrites=true&w=majority`
   )
   .then((resul) => {
     app.listen(PORT, () => {
@@ -16,12 +16,32 @@ mongoose
   })
   .catch((err) => console.log(`conexion fallida ${err}`));
 
+const productSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    number: Number,
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Products", productSchema);
+
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
 
-/* app.get('/',(resq, res)=>{
-    console.log('peticion recibida');
-    res.status(200).sendFile('index.html',{root:__dirname});
-    next()
-}) */
+app.post("/api/v1/products", async (req, res) => {
+  /* console.log("peticion recibida");
+  console.log({ body: req.body });
+  */ /* next(); */
+  const newProduct = new Product(req.body);
 
+  await newProduct
+    .save()
+    .then((resul) => {
+      res.status(201).json({ ok: true });
+    })
+    .catch((err) => console.log(err));
+});
+
+/*  res.status(200).sendFile('index.html',{root:__dirname}); */
 const PORT = process.env.PORT || 4000;
--- public/index.js
+++ public/index.js
@@ -1,11 +1,22 @@
+const inputName = document.querySelector("#productName");
+const inputNumber = document.querySelector("#productNumber");
 
-const inputName = document.querySelector('#productName')
-const inputNumber = document.querySelector('#productNumber')
+console.log({ inputName, inputNumber });
 
-console.log({inputName,inputNumber});
+const boton = document.querySelector("button");
 
-const boton = document.querySelector('button');
+boton.addEventListener("click", (e) => {
+  const name = inputName.value;
+  const number = inputNumber.value;
 
-boton.addEventListener('click',(e) =>{
-        console.log({nombre:inputName.value, precio:inputNumber.value });
-})
+  fetch('/api/v1/products', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name,
+      number,
+    }),
+  });
+});
